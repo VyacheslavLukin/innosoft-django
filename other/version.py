ec3d632ddd6473229e4340f1bb67e11f7ac41d2c
Ilnur Nasibullin <softilnyr16@gmail.com>
2019-06-12 19:15:32 +0300

    X

--- other/version.py
+++ other/version.py
@@ -1,10 +1,12 @@
 import django
+import django_heroku
 import gunicorn
 import psycopg2
 import pytz
 import whitenoise
 import pandas
 print(django.__version__)
+print(django_heroku.__version__)
 print(gunicorn.__version__)
 print(psycopg2.__version__)
 print(pytz.__version__)
