f6a68781ff46dbe32df11ef20ed19e820c6c9e6c
Ilnur Nasibullin <softilnyr16@gmail.com>
2019-06-12 18:42:00 +0300

    set py

--- Algoritma/settings.py
+++ Algoritma/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for Algoritma project.
 
@@ -9,7 +10,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
