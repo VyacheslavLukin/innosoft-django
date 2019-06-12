b88ff8c1d660ec18ad322c4f641fda9b1c3c6323
Ilnur Nasibullin <softilnyr16@gmail.com>
2019-06-12 18:12:18 +0300

    heroku files

---
+++ other/version.py
@@ -0,0 +1,10 @@
+import django
+import gunicorn
+import psycopg2
+import pytz
+import whitenoise
+print(django.__version__)
+print(gunicorn.__version__)
+print(psycopg2.__version__)
+print(pytz.__version__)
+print(whitenoise.__version__)
