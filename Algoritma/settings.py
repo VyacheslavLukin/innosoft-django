d9eef4254cd8347a6ddda451cb0288854f198432
Ilnur Nasibullin <softilnyr16@gmail.com>
2019-06-13 11:51:50 +0300

    logo

--- Algoritma/settings.py
+++ Algoritma/settings.py
@@ -114,11 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/staticfiles/'
+# STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
+# os.makedirs(STATIC_TMP, exist_ok=True)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
